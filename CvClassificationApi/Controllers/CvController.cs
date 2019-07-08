74924d0e5e5251b7dd58b9502dffc92f100da4cc
hamda1996 <39470502+hamda1996@users.noreply.github.com>
2019-07-08 11:16:58 +0200

    Add files via upload

---
+++ CvClassificationApi/Controllers/CvController.cs
@@ -0,0 +1,40 @@
+﻿using System.Collections.Generic;
+using System.IO;
+using System.Threading.Tasks;
+using ClassLibraryCV;
+using Microsoft.AspNetCore.Http;
+using Microsoft.AspNetCore.Mvc;
+
+namespace CvClassificationApi.Controllers
+{
+    [Route("api/file")]
+    [ApiController]
+    public class CvController : ControllerBase
+    {
+        [HttpPost("UploadFiles")]
+        public async Task<IActionResult> Post(List<IFormFile> files)
+        {
+            List<TechnoDuree> listTechnoDuree = new List<TechnoDuree>();
+            CvAnalysisResult cvAnalysisResult = null;
+            List<CvQuality> listCvQuality = new List<CvQuality>();
+            string error = "";
+            foreach (var formFile in files)
+            {
+                if (formFile.Length > 0)
+                {
+                    using (var stream = new MemoryStream())
+                    {
+                        await formFile.CopyToAsync(stream);
+                        cvAnalysisResult = new CvService().TextFromWord(formFile.FileName, stream);
+
+                        listTechnoDuree.AddRange(cvAnalysisResult.ListTechnoDuree);
+                        listCvQuality.AddRange(cvAnalysisResult.ListCvQuality);
+                        error = cvAnalysisResult.Error;
+                    }
+                }
+            }
+            cvAnalysisResult = new CvAnalysisResult(listCvQuality, listTechnoDuree, error);
+            return new JsonResult(cvAnalysisResult);
+        }
+    }
+}
---
+++ CvClassificationApi/Startup.cs
@@ -0,0 +1,62 @@
+using System.Globalization;
+using Microsoft.AspNetCore.Builder;
+using Microsoft.AspNetCore.Hosting;
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.Extensions.Configuration;
+using Microsoft.Extensions.DependencyInjection;
+
+namespace CvClassificationApi
+{
+    public class Startup
+    {
+        public Startup(IConfiguration configuration)
+        {
+            Configuration = configuration;
+        }
+
+        private readonly string MyAllowSpecificOrigins = "_myAllowSpecificOrigins";
+
+        public IConfiguration Configuration { get; }
+
+        // This method gets called by the runtime. Use this method to add services to the container.
+        public void ConfigureServices(IServiceCollection services)
+        {
+            services.AddCors(options =>
+            {
+                options.AddPolicy(MyAllowSpecificOrigins,
+                builder =>
+                {
+                    builder.AllowAnyOrigin()
+                           .AllowAnyHeader()
+                           .AllowAnyMethod();
+                });
+            });
+
+            services.AddMvc().SetCompatibilityVersion(CompatibilityVersion.Version_2_2);
+        }
+
+        // This method gets called by the runtime. Use this method to configure the HTTP request pipeline.
+        public void Configure(IApplicationBuilder app, IHostingEnvironment env)
+        {
+            var cultureInfo = new CultureInfo("fr-FR");
+
+            CultureInfo.DefaultThreadCurrentCulture = cultureInfo;
+            CultureInfo.DefaultThreadCurrentUICulture = cultureInfo;
+
+            if (env.IsDevelopment())
+            {
+                app.UseDeveloperExceptionPage();
+            }
+            else
+            {
+                app.UseHsts();
+            }
+
+            app.UseCors(MyAllowSpecificOrigins);
+
+            app.UseHttpsRedirection();
+
+            app.UseMvc();
+        }
+    }
+}
